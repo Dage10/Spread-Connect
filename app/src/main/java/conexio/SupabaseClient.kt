c4630c8b6757f148a793b5a3c7a45153cf27e504
Dage10 <davidgelmacorral@gmail.com>
2026-07-03 12:48:20 +0200

    Canviar anon key a publishable

--- app/src/main/java/conexio/SupabaseClient.kt
+++ app/src/main/java/conexio/SupabaseClient.kt
@@ -8,7 +8,7 @@ import io.github.jan.supabase.storage.Storage
 object SupabaseClient {
     val client = createSupabaseClient(
         supabaseUrl = "https://fvoouemimuhvwnzbetrl.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2b291ZW1pbXVodnduemJldHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0NTM2NjcsImV4cCI6MjA4MTAyOTY2N30.IZrgoKYzdHRUJBw3vRfh24O3YkJfkFgLTULFzo2t2Zc"
+        supabaseKey = "sb_publishable_z-u5sCNuoK_cU7yEuDpdGw_pZ2F_lRc"
     ) {
         install(Auth)
         install(Postgrest)
